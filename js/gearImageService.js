7085784eb125423691fea6d9b8cefef3635b4add
Peter Son <son.peter2k@gmail.com>
2026-02-01 19:19:41 -0800

    fixed item mapping and icons

--- js/gearImageService.js
+++ js/gearImageService.js
@@ -4,54 +4,55 @@
  */
 
 class GearImageService {
-  static BASE_URL = 'https://maplestory.io/api/GMS/208.2.0/item/';
+  static BASE_URL = 'https://maplestory.io/api/GMS/253/item/';
   
   /**
    * Map current gear IDs to MapleStory item IDs
    * Based on research of MapleStory item database structure
    */
   static GEAR_ID_TO_ITEM_ID = {
-    // AbsoLab Set
-    'absolab-mage-crown': '00002228',
-    'absolab-mage-suit': '00002230',
-    'absolab-mage-shoes': '00002226',
-    'absolab-mage-gloves': '00002227',
-    'absolab-mage-cape': '00002232',
-    'absolab-spellsong-wand': '00002233',
-    'absolab-mage-shoulder': '00002234',
-    
-    // Arcane Umbra Set
-    'arcane-mage-crown': '00002144',
-    'arcane-mage-cape': '00002199',
-    
-    // CRA Set
-    'cra-mage-hat': '00002099',
-    'cra-mage-overall': '00002100',
-    'cra-mage-pants': '00002101',
-    'cra-mage-gloves': '00002098',
-    'cra-mage-shoes': '00002097',
-    
-    // Superior Gollux Set
-    'superior-gollux-ring': '00002057',
-    'superior-gollux-pendant': '00002061',
-    
-    // Sweetwater Set
-    'sweetwater-mage-hat': '00002636',
-    'sweetwater-mage-overall': '00002637',
-    'sweetwater-mage-pants': '00002638',
-    'sweetwater-mage-gloves': '00002639',
-    'sweetwater-mage-shoes': '00002640',
-    
-    // Weapons (will need to add more as needed)
-    'absolab-spellsong-wand': '00002233',
-    'arcane-shade-wand': '00002146',
-    
-    // Common items
-    'common-ring-1': '00020111',
-    'common-earrings-1': '00020112',
-    'common-earrings-2': '00020113',
-    'common-pendant-1': '00020114',
-    'common-shoulder': '00020115'
+    // AbsoLab Set (Level 160)
+    'absolab-mage-crown': '1004423',
+    'absolab-mage-suit': '1052887',
+    'absolab-mage-shoes': '1073032',
+    'absolab-mage-gloves': '1082637',
+    'absolab-mage-cape': '1102794',
+    'absolab-mage-shoulder': '1152176',
+    'absolab-spellsong-wand': '1372222',
+
+    // Arcane Umbra Set (Level 200)
+    'arcane-mage-crown': '1004809',
+    'arcane-mage-cape': '1102941',
+    'arcane-mage-suit': '1053064',
+    'arcane-mage-shoes': '1073159',
+    'arcane-mage-gloves': '1082696',
+    'arcane-mage-shoulder': '1152197',
+    'arcane-umbra-wand': '1372228',
+    'arcane-umbra-staff': '1382265',
+
+    // CRA Set (Level 150) - Royal Dunwitch for Mage
+    'cra-mage-hat': '1003798',
+
+    // Superior Gollux Set (Level 150, 15-star cap)
+    'superior-gollux-ring': '1113075',
+    'superior-gollux-pendant': '1122267',
+    'superior-gollux-earrings': '1032223',
+
+    // Sweetwater Set (Level 160)
+    'sweetwater-hat': '1003976',
+    'sweetwater-suit': '1052669',
+    'sweetwater-shoes': '1072870',
+    'sweetwater-gloves': '1082556',
+    'sweetwater-cape': '1102623',
+    'sweetwater-shoulder': '1152160',
+    'sweetwater-ring': '1113078',
+    'sweetwater-pendant': '1122269',
+    'sweetwater-earrings': '1032224',
+    'sweetwater-belt': '1132247',
+    'sweetwater-tattoo': '1012438',
+    'sweetwater-monocle': '1022211',
+    'sweetwater-wand': '1372195',
+    'sweetwater-staff': '1382231'
   };
 
   /**
